602337337c90d3bc9b39d5d827b91b6bc86b9de1
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-30 13:46:55 -0700

    Add e2e test for users

---
+++ test/e2e/users.test.js
@@ -0,0 +1,30 @@
+const { assert } = require('chai');
+const { request, checkOk } = require('./request');
+const { dropCollection } = require('./db');
+
+const userData = {
+    // name: 'Bikey McBikeface',
+    email: 'bikey@bikeface.com',
+    password: 'myFaceIsABike'
+};
+
+
+describe('Users API', () => {
+    let token;
+
+    beforeEach(() => dropCollection('users'));
+
+    beforeEach(() => {
+        return request
+            .post('/api/users/signup')
+            .send(userData)
+            .then(checkOk)
+            .then(({ body }) => {
+                token = body.token;
+            });
+    });
+
+    it('signs up a user', () => {
+        assert.isDefined(token);
+    });
+});

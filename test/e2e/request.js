e9ff6f68296b9c46c4a28c77b5a68bfe22963a9b
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-30 11:41:13 -0700

    Add necessary additional files for e2e tests

---
+++ test/e2e/request.js
@@ -0,0 +1,30 @@
+const { createServer } = require('http');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const app = require('../../lib/app');
+const server = createServer(app);
+const request = chai.request(server).keepOpen();
+
+const checkOk = res => {
+    if(res.status !== 200) throw new Error('expected http 200 status code');
+    return res;
+};
+
+const save = (path, data, token = null) => {
+    return request
+        .post(`/api/${path}`)
+        .set('Authorization', token)
+        .send(data)
+        .then(checkOk)
+        .then(({ body }) => body);
+};
+
+after(done => server.close(done));
+
+module.exports = {
+    request,
+    save,
+    checkOk
+};

b90ea4fb4bb83a0a43e7dc4daab57534b5490882
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-30 10:16:12 -0700

    Make a unit test for the user model and implement basic test

---
+++ test/unit/user.test.js
@@ -0,0 +1,16 @@
+const { assert } = require('chai');
+const User = require('../../lib/models/user');
+
+const mock = {
+    name: 'Dexter Mongoose',
+    email: 'mongoose@gmail.com',
+    password: 'iHeartMongeese'
+};
+
+describe('User model', () => {
+    it('validates good model', () => {
+        const user = new User(mock);
+        assert.equal(user.email, mock.email);
+        assert.isUndefined(user.password);
+    });
+});
